5b74846b29542e0c64fa586ec9b7a22b27f9e89c
Daniyal Yasin <daniyalyasin93@gmail.com>
2022-09-25 21:57:50 +0500

    added caching

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Usage: -f[Path To xlsx file] -n[Search strings] -n[Search strings]
 
 Get the file form FBR web site: https://www.fbr.gov.pk/download-atl/132041
 
+The program will cache the xlsx file after its fully parsed. This cache will be stored in a pickle file in the same directory, if this file is present when the program is run, the search will be much faster
+
 Example:
 
 python3 search.py -f'/home/dyasin/Downloads/2022091965328335ATL_IT.xlsx' -nGREEN -nPHARMACY
--- search.py
+++ search.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import numpy as np
 import getopt, sys
+from pathlib import Path
+import pickle
 
+def search_taxpayer(df_dict, names):
+     results = []
+     for part in df_dict.keys():
+            masks = (df_dict[part]['NAME'].str.contains(stringtosearch, regex=False).fillna(False) for stringtosearch in names)
+            combined_mask = np.vstack(masks).all(axis=0)
+            results.append(df_dict[part][combined_mask])
+     return results
 
 opts, args = getopt.getopt(
     sys.argv[1:],
@@ -26,17 +35,16 @@ if (len(names) == 0 or file is None):
 
 print('names: ' + str(names))
 print('file: ' + str(file))
+picklepath = Path(file).with_suffix('.pkl')
 
-
-def search_taxpayer(df_dict, names):
-     results = []
-     for part in df_dict.keys():
-            masks = (df_dict[part]['NAME'].str.contains(stringtosearch, regex=False).fillna(False) for stringtosearch in names)
-            combined_mask = np.vstack(masks).all(axis=0)
-            results.append(df_dict[part][combined_mask])
-     return results
-
-
-df = pd.read_excel(file, engine='openpyxl', sheet_name=None)
+if (picklepath.exists()):
+    infile = open(picklepath, 'rb');
+    df = pickle.load(infile)
+    print('Read from pickle')
+else:
+    df = pd.read_excel(file, engine='openpyxl', sheet_name=None)
+    with open(str(picklepath), 'wb') as f:
+        pickle.dump(df, f)
+    print('Writing pickle')
 
 print(search_taxpayer(df, names));
